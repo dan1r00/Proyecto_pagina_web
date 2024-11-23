a2ae113afd74345334edc02abd393217b723a4a5
dan1r00 <danielroque9876@gmail.com>
2024-11-23 23:41:43 +0000

    product.arreg

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ document.addEventListener('DOMContentLoaded', function () {
         ofertasContenedor.style.transition = 'transform 0.3s ease';
         ofertasContenedor.style.transform = `translateX(${desplazamiento}px)`;
 
+        // Resetea cuando alcanza el final duplicado
         if (desplazamiento <= -(totalProductos + totalProductos) * productoWidth) {
             setTimeout(() => {
                 ofertasContenedor.style.transition = 'none';
