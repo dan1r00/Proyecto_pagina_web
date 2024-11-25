17661803123f614bd306ee684e2da4f85772051d
dan1r00 <danielroque9876@gmail.com>
2024-11-25 20:05:40 +0000

    prdb

--- buscar_producto.php
+++ buscar_producto.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "mi_tienda"; 
+$dbname = "productos"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
