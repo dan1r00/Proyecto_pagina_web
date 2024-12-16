1ad8ceae359d14c20c765a75deb9c3a300ae889f
GabrielM1608 <135675604+GabrielM1608@users.noreply.github.com>
2024-12-16 17:31:02 +0000

    gabriel_modo_oscuro

---
+++ modo_oscuro.js
@@ -0,0 +1,14 @@
+const toggleModoOscuro = document.getElementById('toggle-modo-oscuro');
+
+toggleModoOscuro.addEventListener('click', () => {
+    document.body.classList.toggle('modo-oscuro');
+
+    if (document.body.classList.contains('modo-oscuro')) {
+        localStorage.setItem('modoOscuro', 'true');
+    } else {
+        localStorage.setItem('modoOscuro', 'false');
+    }
+});
+if (localStorage.getItem('modoOscuro') === 'true') {
+    document.body.classList.add('modo-oscuro');
+}
