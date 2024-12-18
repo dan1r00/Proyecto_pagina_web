cc2406ea915dc0c3956283d57d4b5125f3387c95
dan1r00 <danielroque9876@gmail.com>
2024-12-18 03:15:49 +0000

    .,.m_o

--- modo_oscuro.js
+++ modo_oscuro.js
@@ -1,6 +1,8 @@
 const toggleModoOscuro = document.getElementById('toggle-modo-oscuro');
-
+const body = document.body;
 toggleModoOscuro.addEventListener('click', () => {
+    body.classList.toggle('modo-oscuro');
+    toggleModoOscuro.textContent = body.classList.contains('modo-oscuro') ? '☀️ Modo Claro' : '🌙 Modo Oscuro';
     document.body.classList.toggle('modo-oscuro');
 
     if (document.body.classList.contains('modo-oscuro')) {
